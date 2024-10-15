92839ca7c1c1841694b10fac52c2e1a78aa7aa59
Busola Dada <dada.busola@gmail.com>
2024-10-15 15:46:39 -0400

    This Python script requests the user to input the two numbers after which the arithmetic calculations are done.

--- test3.py
+++ test3.py
@@ -2,6 +2,7 @@ def calculate_numbers():
     # Ask the user to input two numbers
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
 
     # Calculate the sum, difference, and quotient
     sum_result = num1 + num2
